2e1f041487040465b3a602471216d0bef24b3155
Ajay Dev <devadigaajay1729@gmail.com>
2021-03-30 01:17:04 +0530

    Initail commit
    A cli based quiz app.

---
+++ quiz_data.js
@@ -0,0 +1,26 @@
+//jshint esversion: 6
+
+let data = [
+    {
+        question: 'What is my favourite sports?. : ',
+        answer: 'cricket'
+    },
+    {
+        question: 'What is my favourite subject?. : ',
+        answer: 'math'
+    },
+    {
+        question: "Who is my favourite cricketer?. : ",
+        answer: "ABD"
+    },
+    {
+        question: "Who is my favourite author?. : ",
+        answer: "poorna chandra tejaswi"
+    },
+    {
+        question: "who is my favourite mathematician?. : ",
+        answer: "ramanujan"
+    }
+];
+
+module.exports = data;
